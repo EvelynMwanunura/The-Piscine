34455bde2daf8ab612d67397ec507101a875581c
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-06-22 20:26:36 +0200

    Added global variables from html elements

--- Project-Codewars-Leaderboard/index.mjs
+++ Project-Codewars-Leaderboard/index.mjs
@@ -3,6 +3,13 @@
 // You can delete the contents of the file once you have understood how it
 // works.
 
-export function makeFetchRequest() {
+/*export function makeFetchRequest() {
   return fetch("https://example.com/test");
-}
+}*/
+
+const userInput = decodeURIComponent.getElementById("userInput");
+const submit = document.gerElementById("submit");
+const tableElement = document.getElementById("table");
+const languageSelect = documentGetElementById = ("langaugeSelect");
+
+let userData = []
