ba1e80f8dcaebf2654f103464b2612e7fcabe0d9
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-06-26 08:23:07 +0200

    edited index file

--- Project-Codewars-Leaderboard/index.test.mjs
+++ Project-Codewars-Leaderboard/index.test.mjs
@@ -11,25 +11,46 @@ import test from "node:test";
 import assert from "node:assert";
 import nock from "nock";
 import { fetchData } from "./api.mjs";
+nock.disableNetConnect();
 
-test("mocks a fetch function", async () => {
-  const mockUsername = "Evelyn"
-  
-   // Create a fetch request "mock" using the nock library, which "replaces"
+test("fetchData returns correct user data from codewaars", async () => {
+  const mockUsername = "evelyn122";
+  const mockData = {
+    username: "evelyn122",
+    clan: "Mothers Who Code",
+    ranks: {
+      overall: {
+        score: 1420,
+      },
+      languages: {
+        javascript: {
+          score: 800,
+        },
+        python: {
+          score: 620,
+        },
+      },
+    },
+  };
+
+  // Create a fetch request "mock" using the nock library, which "replaces"
   // real requests with fake ones that we can control in the test using nock
   // functions.
   // In this example, we set up nock so that it looks for GET requests to
   // https://example.com/test (no other URLs will work) and responds with a 200
   // HTTP status code, and the body {"user": "someone"}.
-  const scope = nock("https://example.com").get("/test").reply(200, JSON.stringify({ user: "someone" }));
+  const scope = nock("https://www.codewars.com")
+    .get(`/api/v1/users/${mockUsername}`)
+    .reply(200, mockData);
 
   // Check that the response we got back included the fake body we set up.
-  const response = await makeFetchRequest();
-  const parsedResponse = await response.json();
-  assert(parsedResponse.user === "someone");
+  const data = await fetchData(mockUsername);
 
+  //assert(parsedResponse.user === "someone");
+  assert.deepEqual(data, mockData, "Fetched user data does not match expected");
+  assert(scope.isDone(), "Expected fetch call was not made");
   // Ensure that a fetch request has been replaced by the nock library. This
   // helps ensure that you're not making real fetch requests that don't match
   // the nock configuration.
-  assert(scope.isDone() === true, "No matching fetch request has been made");
+  //assert(scope.isDone() === true, "No matching fetch request has been made");
 });
